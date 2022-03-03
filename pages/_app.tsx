924eb893646ddfdcf477f46a54f416eadc60e76c
TMT <cauke0102@gmail.com>
2022-03-03 22:57:29 +0700

    fixed mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,34 @@
-import type { AppProps } from 'next/app'
+import { FC, useEffect } from 'react';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import theme from '../styles/theme';
+
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
