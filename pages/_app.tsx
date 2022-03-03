c9a6e3c26d0a8ef06eb514f792b5add9db7900ae
TMT <cauke0102@gmail.com>
2022-03-03 23:39:06 +0700

    layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect } from "react";
 
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import theme from '../styles/theme';
-import { AppPropsWithLayout } from '../models';
-import { EmptyLayout } from '../components/layout';
-import { createEmotionCache } from '../utils';
-import { ThemeProvider1 } from '../context/ThemeContext';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import theme from "../styles/theme";
+import { AppPropsWithLayout } from "../models";
+import { EmptyLayout } from "../components/layout";
+import { createEmotionCache } from "../utils";
+import { ThemeProvider1 } from "../context/ThemeContext";
 
-const clientSideEmotionCache = createEmotionCache()
+const clientSideEmotionCache = createEmotionCache();
 export function MyApp(props: AppPropsWithLayout) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const Layout = Component.Layout ?? EmptyLayout;
   useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles?.parentElement?.removeChild(jssStyles);
     }
@@ -26,21 +26,22 @@ export function MyApp(props: AppPropsWithLayout) {
     <>
       <Head>
         <title>My App</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-     
 
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        
 
-        <Component {...pageProps} />
-       
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
-      
     </>
   );
-};
+}
 
 export default MyApp;
